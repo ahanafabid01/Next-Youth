6aec35d579b9deabbd09938173f30b6131ccd4a2
ahanafabid01 <srsrizon665@gmail.com>
2025-04-11 15:01:23 +0600

    first commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import cokkieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoutes.js'; // Use default import
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+// MongoDB connection
+connectDB();
+
+
+app.use(express.json());
+app.use(cors());
+app.use(cokkieParser());
+app.use(cors({credentials: true}));
+app.use('/api/auth', authRouter);
+// API routes
+app.get('/', (req, res) => {
+  res.send('Hello World!!!');
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
